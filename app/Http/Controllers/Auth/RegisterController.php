3e2f620f6f5b523a23933c62c0a1126fd85fbc64
Joel <joeldome17@gmail.com>
2022-08-09 15:10:58 -0500

    feat: Registro de inquilinos (empresas)

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -9,6 +9,17 @@ use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
+use Illuminate\Validation\Rule;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+// MultiTenancy
+use Hyn\Tenancy\Models\Hostname;
+use Hyn\Tenancy\Models\Website;
+use Hyn\Tenancy\Repositories\HostnameRepository;
+use Hyn\Tenancy\Repositories\WebsiteRepository;
+use Stringable;
+
 class RegisterController extends Controller
 {
     /*
@@ -31,6 +42,8 @@ class RegisterController extends Controller
      */
     protected $redirectTo = RouteServiceProvider::HOME;
 
+    protected $tenantName = null;
+
     /**
      * Create a new controller instance.
      *
@@ -41,20 +54,44 @@ class RegisterController extends Controller
         $this->middleware('guest');
     }
 
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-        ]);
+  /**
+   * Show the aplication registration form
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function showRegistrationForm()
+  {
+    return view('auth.register')->with('tenantName', $this->tenantName);
+  }
+
+
+  /**
+   * Get a validator for an incoming registration request.
+   *
+   * @param  array  $data
+   * @return \Illuminate\Contracts\Validation\Validator
+   */
+  protected function validator(array $data)
+  {
+    if (!$this->tenantName) {
+      // Se concatena: hostname.crm_multi_tenancy_dev.test
+      $fqdn = sprintf('%s.%s', $data['fqdn'], env('APP_DOMAIN'));
+      return Validator::make($data, [
+        'name' => ['required', 'string', 'max:255'],
+        'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+        'fqdn' => ['required', 'string', 'max:20', Rule::unique('hostnames')->where(function ($query) use ($fqdn) {
+          return $query->where('fqdn', $fqdn);
+        })],
+        'password' => ['required', 'string', 'min:8', 'confirmed'],
+      ]);
     }
+    return Validator::make($data, [
+      'name' => ['required', 'string', 'max:255'],
+      'email' => ['required', 'string', 'email', 'max:255', 'unique:tenant.users'],
+      'password' => ['required', 'string', 'min:8', 'confirmed'],
+    ]);
+  }
+
 
     /**
      * Create a new user instance after a valid registration.
@@ -70,4 +107,26 @@ class RegisterController extends Controller
             'password' => Hash::make($data['password']),
         ]);
     }
+
+    
+  /**
+   * The user has been registered
+   *
+   * @param  \Illuminate\Http\Request $request
+   * @return mixed $user
+   * @return mixed
+   */
+  protected function registered(Request $request, $user)
+  {
+    if (!$this->tenantName) {
+      $fqdn = sprintf('%s.%s', request('fqdn'), env('APP_DOMAIN'));
+      $website = new Website;
+      $website->uuid = Str::random(10); // No pasarse de 32
+      app(WebsiteRepository::class)->create($website);
+      $hostname = new Hostname();
+      $hostname->fqdn = $fqdn;
+      $hostname = app(HostnameRepository::class)->create($hostname);
+      app(HostnameRepository::class)->attach($hostname, $website);
+    }
+  }
 }
