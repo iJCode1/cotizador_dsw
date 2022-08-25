c2a3206ffa6b05871f2445fdbef82ea373771f1c
Joel <joeldome17@gmail.com>
2022-08-25 15:47:59 -0500

    fix: detectando en middleware de AdminGeneral si se hacen las peticiones desde una web de inquilino o desde web de sistema general

--- app/Http/Middleware/AdminGeneralMiddleware.php
+++ app/Http/Middleware/AdminGeneralMiddleware.php
@@ -10,6 +10,26 @@ use Illuminate\Support\Facades\Auth;
 class AdminGeneralMiddleware
 {
   /**
+   * Variable $tenantName
+   * Almacenara el nombre de una empresa si es que se encuentra dentro de una instacia de empresa(hostname)
+   */
+  protected $tenantName = null;
+
+  /**
+   * Función __construct()
+   * Detectara si se encuentra actualmente en una instancia de empresa(hostname)
+   * Si es el caso, obtendra el fqdn
+   */
+  public function __construct()
+  {
+    $hostname = app(\Hyn\Tenancy\Environment::class)->hostname();
+    if ($hostname) {
+      $fqdn = $hostname->fqdn;
+      $this->tenantName = explode('.', $fqdn)[0];
+    }
+  }
+
+  /**
    * Handle an incoming request.
    *
    * @param  \Illuminate\Http\Request  $request
@@ -18,15 +38,27 @@ class AdminGeneralMiddleware
    */
   public function handle($request, Closure $next)
   {
-    $id = Auth::user()->id; // ID del usuario logueado
-    $consulta = User::join('roles', 'users.rol_id', '=', 'roles.rol_id')
-      ->select('users.name', 'roles.nombre_rol AS NombreRol')
-      ->where('users.id', "=", $id)
-      ->get();
-    if ((auth()->check()) && (auth()->user()->rol_id === 1) && ($consulta[0]->NombreRol === 'Administrador General')) {
-      return $next($request);
+    if (!$this->tenantName) {
+      /**
+       * Si la variable tenantName esta vacía
+       * Se obtiene el ID del usuario autenticado, y se hace una consulta para obtener el nombre de usuario y rol
+       * Se hace una validación de que el usuario este autenticado, que el rol sea 1 y el nombre del rol sea 'Administrador General'
+       */
+      $id = Auth::user()->id; // ID del usuario logueado
+      $consulta = User::join('roles', 'users.rol_id', '=', 'roles.rol_id')
+        ->select('users.name', 'roles.nombre_rol AS NombreRol')
+        ->where('users.id', "=", $id)
+        ->get();
+      if ((auth()->check()) && (auth()->user()->rol_id === 1) && ($consulta[0]->NombreRol === 'Administrador General')) {
+        return $next($request);
+      }
+      return redirect('empresas');
+    } else {
+      /**
+       * Si tenantName tiene un valor, entonces se está dentro del sitio de una empresa(hostname)
+       * Y por ende se le redirige al home ya que no puedo acceder a las rutas de este middleware
+       */
+      return redirect('/home');
     }
-
-    return redirect('empresas');
   }
 }
