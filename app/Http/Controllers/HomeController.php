3bfbc3fa93e5bd40f45efebde1454d7eb844c6e9
Joel <joeldome17@gmail.com>
2022-08-25 15:45:59 -0500

    fix: Obteniendo el usuario logueado y el nombre y rol de dicho usuario diferenciandolo de un usuario de sistema general y de sistema de inquilino(empresa)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,34 +6,58 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\User;
 use App\Models\Roles;
-
+use App\Models\Tenant\User as User2;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+  /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function index()
-    {
+  protected $tenantName = null;
+
+  public function __construct()
+  {
+    $this->middleware('auth');
+    $hostname = app(\Hyn\Tenancy\Environment::class)->hostname();
+    if ($hostname) {
+      $fqdn = $hostname->fqdn;
+      $this->tenantName = explode('.', $fqdn)[0];
+    }
+  }
+
+  /**
+   * Show the application dashboard.
+   *
+   * @return \Illuminate\Contracts\Support\Renderable
+   */
+  public function index()
+  {
+    if (!$this->tenantName) {
       $id = Auth::user()->id; // ID del usuario logueado
       // Consulta entre 2 tablas - Users y Roles
       $consulta = User::join('roles', 'users.rol_id', '=', 'roles.rol_id')
-      ->select('users.name', 'roles.nombre_rol AS NombreRol')
-      ->where('users.id', "=", $id)
-      ->get();
+        ->select('users.name', 'roles.nombre_rol AS NombreRol')
+        ->where('users.id', "=", $id)
+        ->get();
+      // echo($id);
+      echo(Auth::user());
+      echo($consulta);
+      return view('home', ['user' => $consulta]);
+    } else {
+      $id = Auth::user()->id; // ID del usuario logueado
+      // Consulta entre 2 tablas - Users y Roles
+      $consulta = User2::join('roles', 'users.rol_id', '=', 'roles.rol_id')
+        ->select('users.name', 'roles.nombre_rol AS NombreRol')
+        ->where('users.id', "=", $id)
+        ->get();
+      // echo($id);
+      echo(Auth::user());
+      echo "</br>";
       echo($consulta);
       return view('home', ['user' => $consulta]);
     }
+  }
 }
