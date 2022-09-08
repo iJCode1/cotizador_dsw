10cc5c069d46032abe2d16c58f150e672822d527
Joel <joeldome17@gmail.com>
2022-09-08 10:48:50 -0500

    feat: rutas para productos y/o servicios en website de inquilino

--- routes/tenants.php
+++ routes/tenants.php
@@ -2,6 +2,7 @@
 
 // use App\Models\Tenant\Usuario;
 
+use App\Http\Controllers\Tenant\ServiciosController;
 use App\Http\Controllers\Tenant\UsuariosController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,7 +11,7 @@ Route::middleware(['web'])
       ->as('tenant.')
       ->group(function(){
         /**
-         * Rutas para Usuarios (Administrador y Empleados)
+         * Rutas para Usuarios (Administrador)
          */
         Route::get('/empleados', [UsuariosController::class, 'index'])->name('showEmpleados');
         Route::get('/empleado', [UsuariosController::class, 'showRegister'])->name('showRegister');
@@ -19,4 +20,15 @@ Route::middleware(['web'])
         Route::put('/empleado/{usuario_id}', [UsuariosController::class, 'editUser'])->name('editUser');
         Route::get('/empleado/{usuario_id}/delete', [UsuariosController::class, 'deleteUser'])->name('deleteUser');
         Route::get('/empleado/{usuario_id}/activate', [UsuariosController::class, 'activateUser'])->name('activateUser');
+
+        /**
+         * Rutas para productos y/o servicios (Administrador y Empleado)
+         */
+        Route::get('/servicios', [ServiciosController::class, 'index'])->name('showServicios');
+        Route::get('/servicio', [ServiciosController::class, 'showRegisterServicio'])->name('showRegisterServicio');
+        Route::post('/servicio/register', [ServiciosController::class, 'registerServicio'])->name('registerServicio');
+        Route::get('/servicio/{servicio_id}/edit', [ServiciosController::class, 'showEditServicio'])->name('showEditServicio');
+        Route::put('servicio/{servicio_id}', [ServiciosController::class, 'editServicio'])->name('editServicio');
+        Route::get('/servicio/{servicio_id}/delete', [ServiciosController::class, 'deleteServicio'])->name('deleteServicio');
+        Route::get('/servicio/{servicio_id}/activate', [UsuariosController::class, 'activateServicio'])->name('activateServicio');
       });
